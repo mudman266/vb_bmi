a9cb7cd5b0e35c0aee99cbfaa05ecccb6f70fd91
Josh Williams <josh@jwillcreations.com>
2020-12-19 16:54:40 -0500

    Adding existing files

---
+++ Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.picMain = New System.Windows.Forms.PictureBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lstMeasurement = New System.Windows.Forms.ListBox()
+        Me.lblSystem = New System.Windows.Forms.Label()
+        Me.lblHeight = New System.Windows.Forms.Label()
+        Me.lblWeight = New System.Windows.Forms.Label()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.txtWeight = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblBMI = New System.Windows.Forms.Label()
+        CType(Me.picMain, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'picMain
+        '
+        Me.picMain.Image = CType(resources.GetObject("picMain.Image"), System.Drawing.Image)
+        Me.picMain.Location = New System.Drawing.Point(12, 12)
+        Me.picMain.Name = "picMain"
+        Me.picMain.Size = New System.Drawing.Size(487, 426)
+        Me.picMain.TabIndex = 0
+        Me.picMain.TabStop = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Bookman Old Style", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(192, 0)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(241, 35)
+        Me.lblTitle.TabIndex = 1
+        Me.lblTitle.Text = "BMI Calculator"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lblBMI)
+        Me.Panel1.Controls.Add(Me.btnCalculate)
+        Me.Panel1.Controls.Add(Me.txtWeight)
+        Me.Panel1.Controls.Add(Me.txtHeight)
+        Me.Panel1.Controls.Add(Me.lblWeight)
+        Me.Panel1.Controls.Add(Me.lblHeight)
+        Me.Panel1.Controls.Add(Me.lblSystem)
+        Me.Panel1.Controls.Add(Me.lstMeasurement)
+        Me.Panel1.Controls.Add(Me.lblTitle)
+        Me.Panel1.Location = New System.Drawing.Point(514, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(625, 426)
+        Me.Panel1.TabIndex = 2
+        '
+        'lstMeasurement
+        '
+        Me.lstMeasurement.Font = New System.Drawing.Font("Bookman Old Style", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstMeasurement.FormattingEnabled = True
+        Me.lstMeasurement.ItemHeight = 24
+        Me.lstMeasurement.Items.AddRange(New Object() {"Imperial", "Metric"})
+        Me.lstMeasurement.Location = New System.Drawing.Point(389, 52)
+        Me.lstMeasurement.Name = "lstMeasurement"
+        Me.lstMeasurement.Size = New System.Drawing.Size(120, 52)
+        Me.lstMeasurement.TabIndex = 2
+        '
+        'lblSystem
+        '
+        Me.lblSystem.AutoSize = True
+        Me.lblSystem.Font = New System.Drawing.Font("Bookman Old Style", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSystem.Location = New System.Drawing.Point(115, 63)
+        Me.lblSystem.Name = "lblSystem"
+        Me.lblSystem.Size = New System.Drawing.Size(268, 27)
+        Me.lblSystem.TabIndex = 3
+        Me.lblSystem.Text = "Measurement System"
+        '
+        'lblHeight
+        '
+        Me.lblHeight.AutoSize = True
+        Me.lblHeight.Font = New System.Drawing.Font("Bookman Old Style", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeight.Location = New System.Drawing.Point(174, 125)
+        Me.lblHeight.Name = "lblHeight"
+        Me.lblHeight.Size = New System.Drawing.Size(171, 27)
+        Me.lblHeight.TabIndex = 4
+        Me.lblHeight.Text = "Height (in/m)"
+        '
+        'lblWeight
+        '
+        Me.lblWeight.AutoSize = True
+        Me.lblWeight.Font = New System.Drawing.Font("Bookman Old Style", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblWeight.Location = New System.Drawing.Point(170, 177)
+        Me.lblWeight.Name = "lblWeight"
+        Me.lblWeight.Size = New System.Drawing.Size(179, 27)
+        Me.lblWeight.TabIndex = 5
+        Me.lblWeight.Text = "Weight (lb/kg)"
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Font = New System.Drawing.Font("Bookman Old Style", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtHeight.Location = New System.Drawing.Point(351, 123)
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(100, 30)
+        Me.txtHeight.TabIndex = 6
+        '
+        'txtWeight
+        '
+        Me.txtWeight.Font = New System.Drawing.Font("Bookman Old Style", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtWeight.Location = New System.Drawing.Point(355, 175)
+        Me.txtWeight.Name = "txtWeight"
+        Me.txtWeight.Size = New System.Drawing.Size(100, 30)
+        Me.txtWeight.TabIndex = 7
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Enabled = False
+        Me.btnCalculate.Font = New System.Drawing.Font("Bookman Old Style", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(217, 235)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(190, 56)
+        Me.btnCalculate.TabIndex = 8
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblBMI
+        '
+        Me.lblBMI.Font = New System.Drawing.Font("Bookman Old Style", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBMI.Location = New System.Drawing.Point(3, 333)
+        Me.lblBMI.Name = "lblBMI"
+        Me.lblBMI.Size = New System.Drawing.Size(619, 27)
+        Me.lblBMI.TabIndex = 9
+        Me.lblBMI.Text = "BMI: "
+        Me.lblBMI.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1151, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.picMain)
+        Me.Name = "frmMain"
+        Me.Text = "BMI Calculation"
+        CType(Me.picMain, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picMain As PictureBox
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblBMI As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents txtWeight As TextBox
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents lblWeight As Label
+    Friend WithEvents lblHeight As Label
+    Friend WithEvents lblSystem As Label
+    Friend WithEvents lstMeasurement As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class frmMain
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' Get the BMI
+        Dim decBMI As Decimal
+        If (lstMeasurement.SelectedIndex = 0) Then
+            decBMI = Imperial_BMI()
+        Else
+            decBMI = Metric_BMI()
+        End If
+        ' Determine the catagory the BMI falls in
+        Dim strWeightLabel As String
+        If (decBMI < 18.5) Then
+            strWeightLabel = "Underweight"
+        ElseIf (decBMI < 24.9) Then
+            strWeightLabel = "Normal Weight"
+        ElseIf (decBMI < 29.9) Then
+            strWeightLabel = "Overweight"
+        Else
+            strWeightLabel = "Obese"
+        End If
+        lblBMI.Text = "BMI: " & decBMI.ToString("F2") & " Catagorized as: " & strWeightLabel
+    End Sub
+    Private Sub Enable_Button(sender As Object, e As EventArgs) Handles txtWeight.TextChanged, txtHeight.TextChanged, lstMeasurement.SelectedValueChanged
+        ' Enable the calculate button if both text boxes have a value and something is selected in the list box
+        If (Not txtHeight.Text = "" And Not txtWeight.Text = "" And lstMeasurement.SelectedIndex > -1) Then
+            btnCalculate.Enabled = True
+        End If
+    End Sub
+    Private Function Imperial_BMI()
+        Dim decBMI As Decimal
+        Dim decHeight As Decimal
+        Dim decWeight As Decimal
+        Try
+            decHeight = Convert.ToDecimal(txtHeight.Text)
+            decWeight = Convert.ToDecimal(txtWeight.Text)
+        Catch ex As Exception
+            MsgBox("Invalid value found. Check height and weight and try again.")
+        End Try
+        decBMI = (decWeight / (decHeight * decHeight)) * 703
+        Return decBMI
+    End Function
+    Private Function Metric_BMI()
+        Dim decBMI As Decimal
+        Dim decHeight As Decimal
+        Dim decWeight As Decimal
+        Try
+            decHeight = Convert.ToDecimal(txtHeight.Text)
+            decWeight = Convert.ToDecimal(txtWeight.Text)
+        Catch ex As Exception
+            MsgBox("Invalid value found. Check height and weight and try again.")
+        End Try
+        decBMI = decWeight / (decHeight * decHeight)
+        Return decBMI
+    End Function
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Show the splash screen for ~2 seconds
+        Threading.Thread.Sleep(2000)
+    End Sub
+End Class
